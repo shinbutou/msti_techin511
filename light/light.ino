b39fc128bf3e81257fe67ead9c51165cc670fae9
Ian <chenghsi.yang@gmail.com>
2022-12-13 14:05:57 -0800

    optimizing the code for the demonstration video

--- light/light.ino
+++ light/light.ino
@@ -16,7 +16,7 @@ float smoothness_pts = 3333; // Larger this number is, slower the change would b
 float gamma = 0.33; // Affecting the width of peak (more or less darkness)
 float beta = 0.5; // Shifting the gaussian to be symmetric
 
-int timer = 0; // Shifting
+float timer = 0.0; // Shifting
 
 void setup() {
   NeoPixel.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
@@ -40,22 +40,19 @@ void loop() {
   // The breathing light function
   for (int t = 0; t < smoothness_pts ; t++) {
     float brightness_value = 255.0 * (exp( - (pow(((t / smoothness_pts) - beta) / gamma, 2.0)) / 2.0)) - 20;
-    timer += 1;
+    timer += 0.1;
     // Serial.println(brightness_value); // Value-testing
-    if (timer % 100 == 0) {
-      Serial.println(timer); // Value-testing
-    }
+    // if (timer % 100 == 0) {
+    //   Serial.println(timer); // Value-testing
+    // }
 
     for (int pixel = 0; pixel < NUM_PIXELS; pixel++) {
-      if (timer <= 1000) {
+      if (timer <= 800.0) {
         NeoPixel.setPixelColor(pixel, NeoPixel.Color(brightness_value, brightness_value, brightness_value));
-        Serial.println("White");
-      } else if (timer > 1000 && timer <= 2000) { // Blue lighting: 121, 121, 255
+      } else if (timer > 800.0 && timer <= 1500.0) { // Blue lighting: 121, 121, 255
         NeoPixel.setPixelColor(pixel, NeoPixel.Color(brightness_value * 121.0 / 255.0, brightness_value * 121.0 / 255.0, brightness_value));
-        Serial.println("Blue");
       } else { // Yellow Lighting: 255, 150, 5
         NeoPixel.setPixelColor(pixel, NeoPixel.Color(brightness_value, brightness_value * 150.0 / 255.0, brightness_value * 5.0 / 255.0));
-        Serial.println("Yellow");
       }
     }
     NeoPixel.show();
